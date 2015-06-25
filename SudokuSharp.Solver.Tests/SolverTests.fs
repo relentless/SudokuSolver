da43ff63647521b2a3a5352590c249c710f499a2
Grant Crofton <grantac@hotmail.com>
2015-06-25 21:25:01 +0100

    Initial version

---
+++ SudokuSharp.Solver.Tests/SolverTests.fs
@@ -0,0 +1,40 @@
+﻿module SudokuSharp.Solver.Tests
+
+open System
+open SudokuSharp.Solver
+open FsUnit
+open NUnit.Framework
+
+[<Test>]
+let test1()=
+    let ans = Solver().solve (Array2D.create 9 9 (Nullable<int>()))
+    ans.GetLength(0) |> should equal 9
+
+[<Test>]
+let ``solve single row with missing 9``()=
+    let row = [for i in 1..8 do yield Some(i)]@[None] |> List.toArray
+    let expected = [|[for i in 1..9 do yield Some(i)] |> List.toArray|]
+
+    row |> Solver().solveRow |> should equal expected 
+
+[<Test>]
+let ``solve single row with missing 1``()=
+    let row = None::[for i in 2..9 do yield Some(i)] |> List.toArray
+    let expected = [|[for i in 1..9 do yield Some(i)] |> List.toArray|]
+
+    row |> Solver().solveRow |> should equal expected 
+
+[<Test>]
+let ``solve single row with two missing numbers gives both possible solutions``()=
+    let row = [for i in 1..7 do yield Some(i)]@[None;None] |> List.toArray
+    let expected = [|
+        [for i in 1..9 do yield Some(i)] |> List.toArray
+        [for i in 1..7 do yield Some(i)]@[Some(9);Some(8)] |> List.toArray|]
+
+    row |> Solver().solveRow |> should equal expected 
+
+[<Test>]
+let ``try factorial``()=
+    let row = [for i in 1..9 do yield None] |> List.toArray
+    
+    row |> Solver().solveRow |> Array.length|> printfn "%A"
---
+++ SudokuSharp.Solver/Solver.fs
@@ -0,0 +1,40 @@
+﻿namespace SudokuSharp.Solver
+
+open System
+
+type Solver() = 
+    let addNumberToFirstMissing number row =
+        let rec addNumberRecursively number = function
+            | [] -> []
+            | None::tail -> Some(number)::tail
+            | head::tail -> head::(addNumberRecursively number tail)
+        addNumberRecursively number (row |> Array.toList) |> List.toArray
+    
+    member this.solve (problem:Nullable<int>[,]) =
+        Array2D.create 9 9 <| Nullable<int>()
+
+    member this.solveRow (row:int option[]) =
+
+        let tryMissingNumbers row missingNumbers = 
+            missingNumbers 
+            |> Array.map (fun missingNum -> 
+                this.solveRow( row |> addNumberToFirstMissing missingNum ))
+
+        let missingNumbers =
+            [|1..9|]
+            |> Array.filter (fun x -> 
+                not (row |> 
+                    Array.exists (function
+                        | Some(value) -> value = x
+                        | _ -> false)))
+
+        match Seq.length missingNumbers with
+        | 1 -> 
+            [|row |> addNumberToFirstMissing missingNumbers.[0]|]
+        | _ -> 
+            missingNumbers 
+            |> tryMissingNumbers row 
+            |> Array.concat
+                                            
+
+         
